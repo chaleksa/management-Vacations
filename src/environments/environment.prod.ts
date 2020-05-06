33fb769b8bba1819b0c1d56c2058f11d437b1954
fldmousedev <chuprina.al@gmail.com>
2020-05-06 12:27:04 +0300

    Deploy to GitHub pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCfIPLBvrhu99v1BIWSzV7_n9OJlFEqtqw',
+    authDomain: 'management-1a053.firebaseapp.com',
+    databaseURL: 'https://management-1a053.firebaseio.com',
+    projectId: 'management-1a053',
+    storageBucket: 'management-1a053.appspot.com',
+    messagingSenderId: '31196120342'
+    }
 };
